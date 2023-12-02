7cd05ec367c414bea1288a407516bf1b3d3fb281
Sourasish <chrmflr2002@gmail.com>
2023-12-02 19:59:22 +0530

    Added profile tab feature.

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template
 from flask_login import login_required, current_user
+from .services import get_user_info 
 
 views = Blueprint('views', __name__)
 
@@ -7,3 +8,9 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/profile')
+@login_required
+def profile():
+    user_info = get_user_info(current_user.id)
+    return render_template('profile.html', user_info=user_info, user=current_user)
