a930c992a35f9f6644952964c07d4339ef035d45
Tushar Shubhra Pal <paltushar2001@gmail.com>
2023-12-04 15:22:17 +0530

    Add Booking model to website

--- website/models.py
+++ website/models.py
@@ -8,3 +8,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     first_name = db.Column(db.String(150))
     password = db.Column(db.String(150))
+    bookings = db.relationship('Booking')
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10))
+    time = db.Column(db.String(5))
+    service = db.Column(db.String(150))
+    # price = db.Column(db.Numeric(10,2))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
