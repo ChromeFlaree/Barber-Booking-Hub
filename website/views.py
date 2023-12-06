6c3f6c66406f2a1e8bece8ffade9ac63d6569102
Tushar Shubhra Pal <paltushar2001@gmail.com>
2023-12-07 01:49:57 +0530

    Add functionality to profile page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 from .models import Booking
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -14,8 +15,37 @@ def home():
             return redirect(url_for('auth.login'))
     return render_template("home.html", user=current_user)
 
-@views.route('/profile')
+@views.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
     bookings = current_user.bookings
+
+    if request.method == "POST":
+        # check if delete account button was clicked
+        if request.form.get('delete-user'):
+            # delete all bookings associated with user
+            for booking in bookings:
+                db.session.delete(booking)
+            
+            # delete user
+            db.session.delete(current_user)
+            db.session.commit()
+            flash("👋 Your account has been deleted.", category='info')
+            return redirect(url_for('auth.login'))
+        
+        # check if cancel booking button was clicked
+        if request.form.get('cancel-booking'):
+            booking_id = request.form.get('cancel-booking')
+            booking = Booking.query.get(booking_id)
+            db.session.delete(booking)
+            db.session.commit()
+            flash("🗑️ Your appointment has been cancelled!", category='info')
+            return redirect(url_for('views.profile'))
+        
+        # check if edit booking button was clicked
+        if request.form.get('update-booking'):
+            booking_id = request.form.get('update-booking')
+            booking = Booking.query.get(booking_id)
+            return redirect(url_for('booking.update_booking', booking_id=booking.id))
+        
     return render_template('profile.html', user=current_user, bookings=bookings)
